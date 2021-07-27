1f7de4c32909ec759b74a37b1a6dffbc9ece80a6
ShootDomy <ShootDomy@DESKTOP-9CU946S>
2021-07-27 12:39:21 -0500

    Creacion de DAO's

--- EvaluacionWSVintimillaDomenica/src/main/java/ec/edu/ups/EvaluacionWSVintimillaDomenica/dao/ClienteDAO.java
+++ EvaluacionWSVintimillaDomenica/src/main/java/ec/edu/ups/EvaluacionWSVintimillaDomenica/dao/ClienteDAO.java
@@ -1,5 +1,39 @@
 package ec.edu.ups.EvaluacionWSVintimillaDomenica.dao;
 
+import java.util.List;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+import ec.edu.ups.EvaluacionWSVintimillaDomenica.modelo.cliente;
+
+@Stateless
 public class ClienteDAO {
 
+	@PersistenceContext
+	private EntityManager em;
+
+	public void insert(cliente cliente) {
+		em.persist(cliente);
+	}
+
+	public void update(cliente cliente) {
+		em.merge(cliente);
+	}
+
+	public cliente read(String cedula) {
+		cliente c = em.find(cliente.class, cedula);
+		return c;
+	}
+
+	public List<cliente> getCliente(String nombre) {
+		String jpq2 = "SELECT c FROM credito c " + "WHERE nombre LIKE ?1 ";
+
+		Query query = em.createQuery(jpq2, cliente.class);
+
+		List<cliente> clientes = query.getResultList();
+		return clientes;
+	}
 }
--- EvaluacionWSVintimillaDomenica/src/main/java/ec/edu/ups/EvaluacionWSVintimillaDomenica/dao/CreditoDAO.java
+++ EvaluacionWSVintimillaDomenica/src/main/java/ec/edu/ups/EvaluacionWSVintimillaDomenica/dao/CreditoDAO.java
@@ -1,8 +1,11 @@
 package ec.edu.ups.EvaluacionWSVintimillaDomenica.dao;
 
+import java.util.List;
+
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 
 import ec.edu.ups.EvaluacionWSVintimillaDomenica.modelo.credito;
 
@@ -11,17 +14,28 @@ public class CreditoDAO {
 
 	@PersistenceContext
 	private EntityManager em;
-	
+
 	public void insert(credito credito) {
 		em.persist(credito);
 	}
-	
+
 	public void update(credito credito) {
 		em.merge(credito);
 	}
-	
+
 	public credito read(int codigo) {
 		credito c = em.find(credito.class, codigo);
 		return c;
+	}
+
+	public List<credito> getCredito(int codigo) {
+		String jpq2 = "SELECT c FROM credito c ";
+
+		
+		Query query = em.createQuery(jpq2, credito.class);
+		
+
+		List<credito> creditos = query.getResultList();
+		return creditos;
 	}
 }
